f965465561ce2888eb6dc7f2ece21fb20ee86f70
frntprog <frntprog@gmail.com>
2023-10-09 15:57:47 +0300

    app setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, shallowRef, computed, watch, nextTick } from "vue";
+import Chart from "chart.js/auto";
+
+const weights = ref([]);
+
+const weightChartEl = ref(null);
+
+const weightChart = shallowRef(null);
+
+const weightInput = ref(60.0);
+
+const currentWeight = computed(() => {
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 };
+});
+
+const addWeight = () => {
+  weights.value.push({
+    weight: weightInput.value,
+    date: new Date().getTime(),
+  });
+};
+</script>
+
+<template>
+  <main>
+    <h1>Weight Tracker</h1>
+  </main>
+</template>
+
+<style scoped></style>
